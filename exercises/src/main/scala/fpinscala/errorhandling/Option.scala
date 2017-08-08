693e6b9d290837c4f7f534e371603156e78bfb91
Callum White <callum.white@rea-group.com>
2017-08-08 17:36:32 +1000

    Option.scala update

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -53,6 +53,7 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
+  
   def variance(xs: Seq[Double]): Option[Double] = ???
 
   def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
